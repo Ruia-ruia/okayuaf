105a973500fafbf2f6041b0a4b560f91c6ff8da8
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-08-21 00:17:13 +1000

    Create ok.cc

---
+++ ok.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class N_class {
+public:
+      int int_var;
+}
+
+int main() {
+
+//UAF example 1
+   int* int_ptr = new int(3);
+   delete int_ptr 
+
+   int sum = *int_ptr + 4;
+
+   cout << sum;
+
+//UAF example 2
+   N_class* n_object = new Nclass;
+   n_object->int_var = 3;
+   delete n_object
+
+   sum = n_object->int_var + 4;
+   
+   cout << sum;
+}
