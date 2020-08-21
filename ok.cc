ab3bb5fc57554cbf98b3379da506de6a656bd73c
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-08-21 17:22:04 +1000

    Update ok.cc

--- ok.cc
+++ ok.cc
@@ -10,7 +10,7 @@ int main() {
 
 //UAF example 1
    int* int_ptr = new int(3);
-   delete int_ptr 
+   delete int_ptr;
 
    int sum = *int_ptr + 4;
 
@@ -19,7 +19,7 @@ int main() {
 //UAF example 2
    N_class* n_object = new Nclass;
    n_object->int_var = 3;
-   delete n_object
+   delete n_object;
 
    sum = n_object->int_var + 4;
    
